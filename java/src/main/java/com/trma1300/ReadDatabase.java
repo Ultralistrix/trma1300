1e53d32b1c9f9e6251f30d2066be50a204b398c3
FalkenkralleMc <144521681+FalkenkralleMc@users.noreply.github.com>
2026-06-25 12:43:05 +0200

    You can know read the database

--- java/src/main/java/com/trma1300/ReadDatabase.java
+++ java/src/main/java/com/trma1300/ReadDatabase.java
@@ -2,21 +2,40 @@ package com.trma1300;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 
 public class ReadDatabase {
     public static void main() {
         String jdbcUrl = "jdbc:sqlite:java/src/main/resources/trmadatabase.db";
 
+
         try {
             Connection connection = DriverManager.getConnection(jdbcUrl);
-            // Now you can use 'connection' to execute SQL queries.
-            // Don't forget to close the connection when you're done.
-            System.out.println("Conenction successfull");   
+            if (connection == null) return;
+
+            System.out.println("Conenction successfull");
+            Statement statement = connection.createStatement();
+
+            String selectQuery = "SELECT * FROM tasks";
+            ResultSet result = statement.executeQuery(selectQuery);
+
+            System.out.println("The available Data\n");
+
+            while(result.next()){
+                int id = result.getInt("id");
+                String name = result.getString("name");
+                String description = result.getString("description");
+
+                System.out.println(id + " " + name + " " + description);
+            }
+
             connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
     }
 }
